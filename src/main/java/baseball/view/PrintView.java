eb70022960e618646f6b1ff7ebd8b1299f812441
leejohy-0223 <leejohy@naver.com>
2021-11-24 22:33:34 +0900

    feat(PrintView.java): Computer로부터 결과를 출력하는 로직 추가(printResult)

--- src/main/java/baseball/view/PrintView.java
+++ src/main/java/baseball/view/PrintView.java
@@ -7,4 +7,19 @@ public class PrintView {
         System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
+    public static void printResult(int ball, int strike) {
+        StringBuilder sb = new StringBuilder();
+
+        if (ball != 0) {
+            sb.append(ball).append("볼").append(" ");
+        }
+        if (strike != 0) {
+            sb.append(strike).append("스트라이크");
+        }
+        if (sb.length() == 0) {
+            sb.append("낫싱");
+        }
+        System.out.println(sb);
+    }
+
 }
