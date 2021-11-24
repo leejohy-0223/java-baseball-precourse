210dbe9141fe97694d0ef8f962a87796c20eed8e
leejohy-0223 <leejohy@naver.com>
2021-11-25 00:00:47 +0900

    refactor(Validation.java): validateOverlap 메서드 분리

--- src/main/java/baseball/view/Validation.java
+++ src/main/java/baseball/view/Validation.java
@@ -1,10 +1,12 @@
 package baseball.view;
 
-public class Validation {
-    public static void validate(String userInput) {
+import static baseball.Constant.MAX_BALL_COUNT;
 
+public class Validation {
+
+    public static void validate(String userInput) {
         // 길이 검증
-        if (userInput.length() != 3) {
+        if (!validateLength(userInput)) {
             throw new IllegalArgumentException();
         }
 
@@ -19,23 +21,34 @@ public class Validation {
         }
     }
 
+    private static boolean validateLength(String userInput) {
+        return userInput.length() == MAX_BALL_COUNT;
+    }
+
     private static boolean validateNumberFormat(String userInput) {
         return userInput.replaceAll("[1-9]", "").equals("");
     }
 
     private static boolean validateOverlap(String userInput) {
-        boolean[] overlapChk = new boolean[3];
-        for (int j = 0; j < 3; j++) {
-            overlapChk[j] = true;
+        boolean[] overlapChk = new boolean[MAX_BALL_COUNT];
 
-            for (int k = 0; k < 3; k++) {
-                if (overlapChk[k]) continue;
-                if (userInput.charAt(j) == userInput.charAt(k)) {
-                    return false;
-                }
+        for (int j = 0; j < MAX_BALL_COUNT; j++) {
+            overlapChk[j] = true;
+            if (checkSameNumber(userInput, overlapChk, j)) {
+                return false;
             }
             overlapChk[j] = false;
         }
         return true;
+    }
+
+    private static boolean checkSameNumber(String userInput, boolean[] overlapChk, int target) {
+        for (int k = 0; k < MAX_BALL_COUNT; k++) {
+            if (overlapChk[k]) continue;
+            if (userInput.charAt(target) == userInput.charAt(k)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
